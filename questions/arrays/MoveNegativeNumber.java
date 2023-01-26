33256a9210da5c2a3fb4766eaeabe45e0ae80de6
ravi <ravikant@techcrumb.com>
2023-01-26 10:14:47 +0530

    add another way to solve move negative number pbm in array

--- questions/arrays/MoveNegativeNumber.java
+++ questions/arrays/MoveNegativeNumber.java
@@ -11,12 +11,29 @@ public class MoveNegativeNumber {
     public static void main(String[] args) {
 
         int [] a = {-12, 11, -13, -5, 6, -7, 5, -3, -6};
-        check1(a);
+        System.out.println(Arrays.toString(a));
+        check2(a);
+        System.out.println(Arrays.toString(a));
     }
 
     static void check1(int[] a){
         Arrays.sort(a);
         System.out.println(Arrays.toString(a));
+    }
+
+    static void check2(int[] a){
+        int s = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if(a[i]>0){
+                int t = a[i];
+                a[i]=a[s];
+                a[s]=t;
+                s++;
+
+            }
+        }
+
     }
 
 }
