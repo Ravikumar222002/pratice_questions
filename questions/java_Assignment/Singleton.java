ddce7fcd5ddfa165154e2ecfc3866cf0a9ef561d
ravi <ravikant@techcrumb.com>
2023-01-30 13:38:51 +0530

    add early and lazy ways to initalize singleton

--- questions/java_Assignment/Singleton.java
+++ questions/java_Assignment/Singleton.java
@@ -2,25 +2,47 @@ package questions.java_Assignment;
 
 // 9. WAP to create singleton class.
 
-class Test {    // singleton class
+class SingletonEarlyInitialize {    // singleton class
 
-    static Test t = new Test();  // make 't' static bcoz, getInstance() method are static i.e static method
+    static SingletonEarlyInitialize t = new SingletonEarlyInitialize();  // make 't' static bcoz, getInstance() method are static i.e static method
                                  // use only static variable or member
-
-    private Test (){
+    private SingletonEarlyInitialize(){
         System.out.println("constructor" + this);
     }
 
-    static Test getInstance(){  // return 't' address
+    static SingletonEarlyInitialize getInstance(){  // return 't' address
         return t;
+    }
+}
+
+class SingletonLazyInitialize{
+
+    private static SingletonLazyInitialize  t = null;
+
+    private SingletonLazyInitialize (){
+        System.out.println("constructor" + this);
+    }
+
+    static SingletonLazyInitialize getInstance(){
+         if(t==null){
+             t=new SingletonLazyInitialize();
+         }
+         return t;
     }
 }
 
 public class Singleton {
 
     public static void main(String[] args) {
-        Test t1 = Test.getInstance();
+        SingletonEarlyInitialize t1 = SingletonEarlyInitialize.getInstance();
         System.out.println("t1 :" + t1);
+
+
+         SingletonLazyInitialize t10 = SingletonLazyInitialize.getInstance();
+        System.out.println("t10 :" + t10);
+        SingletonLazyInitialize t20 = SingletonLazyInitialize.getInstance();
+        System.out.println("t20 :" + t20);
+
     }
 
 }
