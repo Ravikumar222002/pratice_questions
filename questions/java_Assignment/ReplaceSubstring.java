323de434109693d74989664246821825a2d0f067
ravi <ravikant@techcrumb.com>
2023-02-02 11:39:18 +0530

    refactored ReplaceString question

--- questions/java_Assignment/ReplaceSubstring.java
+++ questions/java_Assignment/ReplaceSubstring.java
@@ -1,36 +1,16 @@
 package questions.java_Assignment;
 
 // Q1. Write a program to replace a substring inside a string with other string ?
-//Input: S = “aba b aba”, S1 = “aba”, S2 = “a”
-//        Output: aba
 
 public class ReplaceSubstring {
 
     public static void main(String[] args) {
-
-//        char chg='b';
-//        char key='d';
-//        char[] m = s.toCharArray();
-      //  check(m,key,chg);
-      //  check1(s,"a","d");
 
         String s="abcde";
         String tar="cd";
         String chg="fgfd";
 
         System.out.println(check1(s,tar,chg));
-
-
-
-    }
-
-    static void check(char[] m, char key,char chg){
-        for (int i = 0; i < m.length; i++) {
-            if(m[i] == chg){
-                m[i]=key;
-            }
-        }
-        System.out.println("result :" + String.valueOf(m));
     }
 
     static String check1(String s, String target, String repl ){
